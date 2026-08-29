20043f5c9862dde2294e4cb4a8e7dcbb03ca1143
Harsh-1324 <harshhgupta1324@gmail.com>
2026-08-30 01:06:05 +0530

    Add temperatureSpike function to handle temperature spikes

--- audit-trail/server/src/commands/TemperatureSpike.command.js
+++ audit-trail/server/src/commands/TemperatureSpike.command.js
@@ -1,1 +1,11 @@
 // TemperatureSpike.command.js
+
+function temperatureSpike(shipment, temperature) {
+    return {
+        ...shipment,
+        temperature: temperature,
+        status: "TEMPERATURE_SPIKE"
+    };
+}
+
+module.exports = temperatureSpike;
