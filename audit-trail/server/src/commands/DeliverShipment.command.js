02e0e029aa58e502912bceaeb085e82b076df1c2
Harsh-1324 <harshhgupta1324@gmail.com>
2026-08-29 21:03:38 +0530

    Update DeliverShipment.command.js

--- audit-trail/server/src/commands/DeliverShipment.command.js
+++ audit-trail/server/src/commands/DeliverShipment.command.js
@@ -1,1 +1,11 @@
 // DeliverShipment.command.js
+
+function deliverShipment(shipment) {
+    return {
+        ...shipment,
+        status: "DELIVERED",
+        deliveredAt: new Date()
+    };
+}
+
+module.exports = deliverShipment;
