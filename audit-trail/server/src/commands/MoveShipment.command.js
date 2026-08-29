c907f7c0fd506f666e443c07764e05211f01543b
Harsh-1324 <harshhgupta1324@gmail.com>
2026-08-30 01:06:16 +0530

    Update MoveShipment.command.js

--- audit-trail/server/src/commands/MoveShipment.command.js
+++ audit-trail/server/src/commands/MoveShipment.command.js
@@ -1,1 +1,11 @@
-// MoveShipment.command.js
+// TemperatureSpike.command.js
+
+function temperatureSpike(shipment, temperature) {
+    return {
+        ...shipment,
+        temperature: temperature,
+        status: "TEMPERATURE_SPIKE"
+    };
+}
+
+module.exports = temperatureSpike;
