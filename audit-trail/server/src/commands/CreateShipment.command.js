b4c7a626e704f64d217a9ba9c97d36d46ba8d5f8
Harsh-1324 <harshhgupta1324@gmail.com>
2026-08-29 21:03:26 +0530

    Update CreateShipment.command.js

--- audit-trail/server/src/commands/CreateShipment.command.js
+++ audit-trail/server/src/commands/CreateShipment.command.js
@@ -1,1 +1,76 @@
+Skip to content
+Sujal90-16
+Audit_Trail
+Repository navigation
+Code
+Issues
+Pull requests
+Agents
+Actions
+Projects
+Wiki
+Security and quality
+Insights
+Settings
+Files
+Go to file
+t
+T
+commands content loaded
+audit-trail
+client
+docs
+server
+prisma
+schema.prisma
+seed.js
+src
+commands
+ArrivePort.command.js
+CreateShipment.command.js
+DeliverShipment.command.js
+LoadShipment.command.js
+MoveShipment.command.js
+TemperatureSpike.command.js
+config
+constants
+controllers
+eventStore
+queries
+routes
+utils
+app.js
+index.js
+.env
+package.json
+.env.example
+.gitignore
+LICENSE
+README.md
+package.json
+README.md
+Audit_Trail/audit-trail/server/src/commands
+/
+CreateShipment.command.js
+in
+main
+
+Edit
+
+Preview
+Indent mode
+
+Spaces
+Indent size
+
+2
+Line wrap mode
+
+No wrap
+Editing CreateShipment.command.js file contents
+  1
+  2
 // CreateShipment.command.js
+
+Use Control + Shift + m to toggle the tab key moving focus. Alternatively, use esc then tab to move to the next interactive element on the page.
+commands content loaded
