b32b342719b13a6597dcfc8389ea768cdf027fab
Harsh-1324 <harshhgupta1324@gmail.com>
2026-08-31 23:10:24 +0530

    Update GetAnalytics.query.js

--- audit-trail/server/src/queries/GetAnalytics.query.js
+++ audit-trail/server/src/queries/GetAnalytics.query.js
@@ -1,1 +1,13 @@
-// GetAnalytics.query.js
+module.exports = {
+    name: "GetAnalytics",
+    description: "Get shipment analytics",
+
+    execute: async (context) => {
+        return {
+            totalShipments: 100,
+            delivered: 70,
+            inTransit: 20,
+            pending: 10
+        };
+    }
+};
