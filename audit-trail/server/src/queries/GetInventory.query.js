fcee288919eb334e5d4d62def4847579875e9f22
Harsh-1324 <harshhgupta1324@gmail.com>
2026-08-31 23:10:37 +0530

    Update GetInventory.query.js

--- audit-trail/server/src/queries/GetInventory.query.js
+++ audit-trail/server/src/queries/GetInventory.query.js
@@ -1,1 +1,13 @@
-// GetInventory.query.js
+module.exports = {
+    name: "GetInventory",
+    description: "Get current inventory details",
+
+    execute: async (context) => {
+        return {
+            totalItems: 500,
+            availableItems: 350,
+            reservedItems: 100,
+            outOfStock: 50
+        };
+    }
+};
