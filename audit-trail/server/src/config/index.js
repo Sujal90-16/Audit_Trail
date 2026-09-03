c03497c150af828a97efaf82f2703abeedc71574
Harsh-1324 <harshhgupta1324@gmail.com>
2026-09-03 16:42:46 +0530

    Add PORT and other needed part to configuration

--- audit-trail/server/src/config/index.js
+++ audit-trail/server/src/config/index.js
@@ -5,3 +5,10 @@ const config = {
 };
 
 
+const config = {
+    PORT: process.env.PORT || 5000,
+    NODE_ENV: process.env.NODE_ENV || "development",
+    DATABASE_URL: process.env.DATABASE_URL || ""
+};
+
+
