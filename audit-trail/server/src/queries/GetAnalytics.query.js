c59bc29e02026275dc1bc9e2495d26f528a2dd1c
Harsh-1324 <harshhgupta1324@gmail.com>
2026-09-12 00:41:21 +0530

    worked and implemented the src part of queries GetAnalytics query module exports

--- audit-trail/server/src/queries/GetAnalytics.query.js
+++ audit-trail/server/src/queries/GetAnalytics.query.js
@@ -10,18 +10,6 @@ module.exports = {
             pending: 10
         };
     }
-};odule.exports = {
-    name: "GetAnalytics",
-    description: "Get shipment analytics",
-
-    execute: async (context) => {
-        return {
-            totalShipments: 100,
-            delivered: 70,
-            inTransit: 20,
-            pending: 10
-        };
-    }
-
+};
 
 
